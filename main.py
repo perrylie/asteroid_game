422ec95f65570c21b148d578873c6b3d47516fee
perrylie <perry.liedtke@gmail.com>
2026-02-01 15:20:14 +0100

    shots can destroy asteroids

--- main.py
+++ main.py
@@ -41,6 +41,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         
         pygame.display.flip()
         
