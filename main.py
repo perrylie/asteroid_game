fa7f31af1ef4fe84ac93c7a630f6b2f938d1dca1
perrylie <perry.liedtke@gmail.com>
2026-02-01 13:08:24 +0100

    added deltatime

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -12,6 +14,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        print(dt)
     
 
 if __name__ == "__main__":
