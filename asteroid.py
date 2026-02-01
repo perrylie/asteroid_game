b8cb9c533c99ffb0f6fb35c813173bb63a51d6fb
perrylie <perry.liedtke@gmail.com>
2026-02-01 15:34:47 +0100

    asteroids not split into smaller asteroids when destroyed

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import *
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20, 50)
+            first_new_asteroid_movement = self.velocity.rotate(angle)
+            second_new_asteroid_movement = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            self.kill()
+            first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            second_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            first_asteroid.velocity = first_new_asteroid_movement * 1.2
+            second_asteroid.velocity = second_new_asteroid_movement * 1.2
